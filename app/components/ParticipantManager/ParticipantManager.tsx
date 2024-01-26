f99c574efb00195c9c67bce855e04e1f0bc1dad9
Fedevs <fededevsit@gmail.com>
2024-01-26 23:51:52 +1100

    adding validations and ux to the add button

--- app/components/ParticipantManager/ParticipantManager.tsx
+++ app/components/ParticipantManager/ParticipantManager.tsx
@@ -1,5 +1,5 @@
 'use client';
-import { useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import { useStore } from '@/app/store/useStore';
 import ParticipantsList from '@/app/components/ParticipantsList/ParticipantsList';
 
@@ -7,6 +7,11 @@ export default function ParticipantManager() {
   const { participants, addParticipant } = useStore();
   const [newName, setNewName] = useState<string>('');
   const [error, setError] = useState<string>('');
+  const inputRef = useRef<HTMLInputElement | null>(null);
+
+  useEffect(() => {
+    inputRef?.current?.focus();
+  }, [participants]);
 
   const handleChange = (
     e: React.ChangeEvent<HTMLInputElement>
@@ -14,32 +19,41 @@ export default function ParticipantManager() {
     setNewName(e.target.value);
   };
 
+  const isUniqueName = (name: string) => {
+    return !participants.some(
+      (participant) =>
+        participant.name.toLowerCase().trim() ===
+        name.toLowerCase().trim()
+    );
+  };
+
   const handleAddParticipantClick = () => {
     setError('');
-    if (isUniqueName(newName)) addParticipant(newName);
-    else {
+    if (!newName.trim()) {
+      setError('Please enter a valid participant name.');
+    } else if (isUniqueName(newName)) {
+      addParticipant(newName);
+      setNewName('');
+    } else {
       setError(
-        'There is already a participant with the same name. If someone else shares the same name, consider using a nickname.'
+        'A participant with the same name already exists. Consider using a nickname.'
       );
     }
   };
 
-  const isUniqueName = (name: string) => {
-    return !participants.some(
-      (participant) =>
-        participant.name.toLowerCase() ===
-        name.toLowerCase()
-    );
-  };
-
   return (
     <section data-testid='participant-manager'>
-      <label htmlFor='newName'>Add participant</label>
+      <label htmlFor='newName'>Add participant: </label>
       <input
         type='text'
+        ref={inputRef}
         id='newName'
+        name='newName'
         value={newName}
         onChange={handleChange}
+        onKeyDown={(e) =>
+          e.key === 'Enter' && handleAddParticipantClick()
+        }
       />
       <button
         type='button'
