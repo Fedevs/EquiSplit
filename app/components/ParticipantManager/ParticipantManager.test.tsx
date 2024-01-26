ed9ad3201105fbf07c2e3caa1405b5dc8b0eb441
Fedevs <fededevsit@gmail.com>
2024-01-26 23:52:49 +1100

    some test to ParticipantManager component

--- app/components/ParticipantManager/ParticipantManager.test.tsx
+++ app/components/ParticipantManager/ParticipantManager.test.tsx
@@ -1,9 +1,49 @@
-import { render, screen } from '@testing-library/react';
+// ParticipantManager.test.tsx
+import {
+  render,
+  screen,
+  fireEvent,
+} from '@testing-library/react';
 import ParticipantManager from '@/app/components/ParticipantManager/ParticipantManager';
 
 describe('ParticipantManager', () => {
   test('renders without errors', () => {
     render(<ParticipantManager />);
     expect(screen.getByTestId('participant-manager'));
+  });
+
+  test('adds a participant when clicking "Add" button', () => {
+    render(<ParticipantManager />);
+
+    // Write a name on the input related to this label
+    const input = screen.getByLabelText('Add participant');
+    fireEvent.change(input, {
+      target: { value: 'Lio Messi' },
+    });
+
+    // Click on button "Add"
+    const addButton = screen.getByText('Add');
+    fireEvent.click(addButton);
+
+    // Find that name
+    expect(
+      screen.getByText('Lio Messi')
+    ).toBeInTheDocument();
+  });
+
+  test('displays an error message for duplicate names', () => {
+    render(<ParticipantManager />);
+
+    const input = screen.getByLabelText('Add participant');
+    fireEvent.change(input, {
+      target: { value: 'Lio Messi' },
+    });
+
+    const addButton = screen.getByText('Add');
+    fireEvent.click(addButton);
+
+    expect(
+      screen.getByText(/already a participant/i)
+    ).toBeInTheDocument();
   });
 });
