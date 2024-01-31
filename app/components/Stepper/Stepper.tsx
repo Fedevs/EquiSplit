78cf947c346228674bedd42fbb0bc00900c7798c
Fedevs <fededevsit@gmail.com>
2024-01-31 14:12:40 +1100

    test warning

--- app/components/Stepper/Stepper.tsx
+++ app/components/Stepper/Stepper.tsx
@@ -19,6 +19,7 @@ const steps = [
 
 export default function HorizontalLinearStepper() {
   const { activeStep, setActiveStep, reset } = useStore();
+  console.log('test linter');
 
   const handleNext = () => {
     setActiveStep(activeStep + 1);
